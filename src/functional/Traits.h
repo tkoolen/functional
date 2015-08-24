7ed66ef27e34c68f8d3b140cbbfc51e26922003f
Twan Koolen <koolen.twan@gmail.com>
2015-08-24 03:12:17 -0400

    Fix.

--- src/functional/Traits.h
+++ src/functional/Traits.h
@@ -11,16 +11,12 @@ template<class T, class Enable = void>
 struct is_differentiable
 {
   static const bool value = false;
-
-  constexpr operator bool() const noexcept { return value; };
 };
 
 template<class T>
 struct is_differentiable<T, typename std::enable_if<!std::is_void<decltype(std::declval<T>().derivative())>{}>::type>
 {
   static const bool value = true;
-
-  constexpr const operator bool() const noexcept { return value; };
 };
 
 #endif //FUNCTIONAL_TRAITS_H
